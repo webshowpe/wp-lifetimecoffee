b8ba2562930baafc91555239a0fa4d6c621613e1
Diego Amorin <hola@diegoamorin.com>
2023-12-27 18:23:52 -0500

    Agregar favicon y comprimir imagenes

--- header.php
+++ header.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lifetime Coffee</title>
+    <link
+      rel="icon"
+      type="image/x-icon"
+      href="<?php echo get_template_directory_uri(); ?>/assets/imgs/faviconlifetime.png"
+    />
     <?php wp_head(); ?>
 
     <style>
